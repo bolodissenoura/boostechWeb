1df8ccfd93b3b4719f54e70de34cdacfdea7102d
Daniel Lima <suamaemedeu@outlook.com>
2021-05-23 22:38:28 -0300

    menu hamburguer

--- main.js
+++ main.js
@@ -1,54 +1,5 @@
-const menuBtn = document.querySelector('.menu-btn');
-let menuOpen = false;
-menuBtn.addEventListener('click', () => {
-  if(!menuOpen) {
-    menuBtn.classList.add('open');
-    menuOpen = true;
-  } else {
-    menuBtn.classList.remove('open');
-    menuOpen = false;
-  }
+$(document).ready(function (){
+  $('.hamburguer').click(function () {
+      $(".menu").toggleClass('active');
+  });
 });
-
-//Debounce do Lodash
-debounce = function(func,wait,immediate){
-  var timeout;
-  return function(){
-    var context = this, args = arguments;
-    var later = function(){
-      timeout = null;
-      if (!immediate) func.apply(context,args);
-    };
-    var callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context,args);
-  };
-};
-
-
-(function () {
-var $target = $('.anime'),
-    animationClass = 'anime-start';
-    offset = $(window).height() * 5/4;
-
-  function animeScroll(){
-    var documentTop = $(document).scrollTop();
-    
-    $target.each(function() {
-      var itemTop = $(this).offset().top;
-      if(documentTop > itemTop - offset){
-        $(this).addClass(animationClass);
-      } else {
-        $(this).removeClass(animationClass);
-      }
-    })
-  }
-
-animeScroll()
-
-$(document).scroll(debounce(function(){
-  animeScroll();
-  console.log('teste');  
-},200));
-}());
